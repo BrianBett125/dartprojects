919d7fa1f434512f7027a841b277f4324f3c875a
Brian Kipkoech Bett <brianbett756@gmail.com>
2026-03-29 23:43:41 +0300

    Remove duplicate main function in hello.dart
    Removed duplicate main function definitions.

--- dart-projects/hello.dart
+++ dart-projects/hello.dart
@@ -3,3 +3,20 @@ void main()
 {
 print('hello, dart');
 }
+
+
+
+
+// my first dart program//
+void main()
+{
+print('hello, dart');
+}// my first dart program//
+void main()
+{
+print('hello, dart');
+}// my first dart program//
+void main()
+{
+print('hello, dart');
+}
